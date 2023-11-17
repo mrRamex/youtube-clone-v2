3a8e55e24b683ad71cf370c20e468980d6a3b390
mrRamex <143888146+mrRamex@users.noreply.github.com>
2023-11-17 09:00:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header/Header'
+import Header2 from './Components/Header2/Header2'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Header2/>
+    </div>
+  );
+}
+
+export default App;
